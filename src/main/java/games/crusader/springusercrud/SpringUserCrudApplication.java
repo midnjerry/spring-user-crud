b2e60880f09eca0a8b394812b5a4e98a16419705
Jerry Balderas <JerryBalderas@appnovation.com>
2019-02-20 22:35:08 -0600

    Basic Project Setup

---
+++ src/main/java/games/crusader/springusercrud/SpringUserCrudApplication.java
@@ -0,0 +1,13 @@
+package games.crusader.springusercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserCrudApplication.class, args);
+	}
+
+}
